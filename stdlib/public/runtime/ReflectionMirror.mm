001fb6b23a3abba7a09f0122290091142da803dd
Joe Groff <jgroff@apple.com>
2018-03-01 16:04:56 -0800

    Knock out some inappropriate "unreachable"s in the runtime.
    Future Swifts may add new metadata kinds, so it isn't appropriate to crash when we see one. In the case of reflection, we can fall back to opaque behavior.

--- stdlib/public/runtime/ReflectionMirror.mm
+++ stdlib/public/runtime/ReflectionMirror.mm
@@ -633,21 +633,24 @@ auto call(OpaqueValue *passedValue, const Metadata *T, const Metadata *passedTyp
       LLVM_FALLTHROUGH;
     }
 
-    /// TODO: Implement specialized mirror witnesses for all kinds.
-    case MetadataKind::Function:
-    case MetadataKind::Existential: {
-      OpaqueImpl impl;
-      return call(&impl);
-    }
 
     // Types can't have these kinds.
     case MetadataKind::HeapLocalVariable:
     case MetadataKind::HeapGenericLocalVariable:
     case MetadataKind::ErrorObject:
       swift::crash("Swift mirror lookup failure");
-  }
 
-  swift_runtime_unreachable("Unhandled MetadataKind in switch.");
+    /// TODO: Implement specialized mirror witnesses for all kinds.
+    case MetadataKind::Function:
+    case MetadataKind::Existential: {
+      break;
+    }
+  }
+  
+  // If we have an unknown kind of type, or a type without special handling,
+  // treat it as opaque.
+  OpaqueImpl impl;
+  return call(&impl);
 }
 
 } // end anonymous namespace
@@ -738,7 +741,7 @@ const char *swift_OpaqueSummary(const Metadata *T) {
       return "(ErrorType Object)";
   }
 
-  swift_runtime_unreachable("Unhandled MetadataKind in switch.");
+  return "(Unknown)";
 }
 
 #if SWIFT_OBJC_INTEROP
